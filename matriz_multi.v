b24d17c294dce155af33293164cb4d3994568276
Felipe Amorim <119305066+Felipeacs05@users.noreply.github.com>
2025-03-26 09:06:55 -0300

    Atualizar o matriz_multi.v

--- matriz_multi.v
+++ matriz_multi.v
@@ -1,28 +1,32 @@
-module matriz_multi(
-    input wire [255:0] matriz_a,
-    input wire [255:0] matriz_b,
-    output wire [255:0] data_c
+module MultiplicadorMatriz (
+    input signed [199:0] matriz_a, // Matriz 5x5 achatada (cada elemento com 8 bits)
+    input signed [199:0] matriz_b, // Matriz 5x5 achatada (cada elemento com 8 bits)
+    input [7:0] tamanho,
+    input clock,
+    output reg signed [199:0] resultado // Matriz 5x5 achatada (cada elemento com 16 bits)
 );
 
-    wire [15:0] parcial[0:4][0:4][0:4]; // Armazena os produtos parciais
-    wire [15:0] soma[0:4][0:4]; // Armazena as somas intermediárias
+    reg [7:0] linha;
+    integer i, j;
 
-    genvar i, j, k;
-    
-    generate
-        for (i = 0; i < 5; i = i + 1) begin : linha
-            for (j = 0; j < 5; j = j + 1) begin : coluna
-                for (k = 0; k < 5; k = k + 1) begin : multiplicacao
-                    assign parcial[i][j][k] = 
-                        matriz_a[(8*k) + (40*i) +: 8] * matriz_b[(40*k) + (8*j) +: 8];
-                end
-                assign soma[i][j] = 
-                    parcial[i][j][0] + parcial[i][j][1] + 
-                    parcial[i][j][2] + parcial[i][j][3] + 
-                    parcial[i][j][4];
-                assign data_c[(8*j) + (40*i) +: 8] = soma[i][j][7:0]; // Pega apenas 8 bits
+    function integer indice;
+        input integer linha, coluna;
+        indice = 8 * (coluna + 5 * linha);
+    endfunction
+
+    always @(posedge clock) begin
+        for (j = 0; j < 5; j = j + 1) begin
+            resultado[indice(linha, j) +: 8] = 0;
+            for (i = 0; i < 5; i = i + 1) begin
+                resultado[indice(linha, j) +: 8] = resultado[indice(linha, j) +: 8] +
+                    matriz_a[indice(linha, i) +: 8] * matriz_b[indice(i, j) +: 8];
             end
         end
-    endgenerate
-
+        
+        if (linha == 4) begin
+            linha <= 0;
+        end else begin
+            linha <= linha + 1;
+        end
+    end
 endmodule
